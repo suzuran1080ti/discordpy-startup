a3e59a055a0b178101fb91083f6b0d5844d4420e
すずらん <83231128+suzuran1080ti@users.noreply.github.com>
2021-04-29 20:56:52 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -14,51 +14,12 @@ async def on_command_error(ctx, error):
 
 
 @bot.command()
-async def tutorial(ctx):
-    await ctx.send('このサーバーの仕組みについて説明します。')
-    
-    bot.run(token)
+async def ping(ctx):
+    await ctx.send('pong')
 
-CHANNEL_ID = 836220806761021514 # 任意のチャンネルID(int)
 
-# 任意のチャンネルで挨拶する非同期関数を定義
-async def greet():
-    channel = client.get_channel(CHANNEL_ID)
-    await channel.send('起動しました')
-
-# bot起動時に実行されるイベントハンドラを定義
-@client.event
-async def on_ready():
-    await greet() # 挨拶する非同期関数を実行
-
-ID_CHANNEL_WELCOME = 836220806761021514 # 入室用チャンネルのID(int)
-ID_ROLE_WELCOME = 836940685184860180 # 付けたい役職のID(int)
-EMOJI_WELCOME = '✅' # 対応する絵文字
-
-# 役職を付与する非同期関数を定義
-async def grant_role(payload):
-    # 絵文字が異なる場合は処理を打ち切る
-    if payload.emoji.name != EMOJI_WELCOME: 
-        return
-
-    # チャンネルが異なる場合は処理を打ち切る
-    if payload.channel_id != ID_CHANNEL_WELCOME:
-        return
-
-    # Member オブジェクトと Role オブジェクトを取得して役職を付与
-    member = payload.member
-    role = guild.get_role(ID_ROLE_WELCOME)
-    await member.add_roles(role)
-    return member
-
-# リアクション追加時に実行されるイベントハンドラを定義
-@client.event
-async def on_raw_reaction_add(payload):
-    # 役職を付与する非同期関数を実行して Optional[Member] オブジェクトを取得
-    member = await grant_role(payload)
-    if member is not None: # 役職を付与したメンバーがいる時
-        text = f'{member.mention} ようこそ3MEサーバーへ！認証が終わるまで、サーバーを見学してお待ちください！'
-        await client.get_channel(ID_CHANNEL_WELCOME).send(text)
+bot.run(token)
+© 2021 GitHub, Inc.
 
     
     
