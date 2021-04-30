f066a9f7a86204693cfaee0f35111702966206f6
すずらん <83231128+suzuran1080ti@users.noreply.github.com>
2021-04-30 11:32:20 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -12,12 +12,20 @@ async def on_command_error(ctx, error):
     error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
     await ctx.send(error_msg)
 
+@bot.command()
+async def begin(ctx):
+    await ctx.send('3MEサーバーへようこそ！まずは「/tutorial」と入力して、サーバーについて学びましょう！')
+
 
 @bot.command()
 async def tutorial(ctx):
     await ctx.send('チュートリアルを開始します。')
+    await ctx.send('知りたい内容があれば以下のコマンドを入力してください！')
     
-   
+@bot.command()
+async def server(ctx):
+    await ctx.send('このサーバーは有志によって作られたクラスサーバーです。ここでは課題や予定の管理をはじめとし、様々なコミュニケーションを図れます。')
+     
 
 bot.run(token)
 
