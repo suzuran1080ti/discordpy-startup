72348e110676fb5b5f8a3d4a8459cdaadeddc24b
すずらん <83231128+suzuran1080ti@users.noreply.github.com>
2021-05-01 02:15:51 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -2,43 +2,6 @@ from discord.ext import commands
 import os
 import traceback
 
-bot = commands.Bot(command_prefix='/')
-token = os.environ['DISCORD_BOT_TOKEN']
-
-
-@bot.event
-async def on_command_error(ctx, error):
-    orig_error = getattr(error, "original", error)
-    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
-    await ctx.send(error_msg)
-
-@bot.command()
-async def begin(ctx):
-    await ctx.send('3MEサーバーへようこそ！')
-    await ctx.send('まずは「/tutorial」と入力して、サーバーについて学びましょう！')
-
-
-@bot.command()
-async def tutorial(ctx):
-    await ctx.send('チュートリアルを開始します。')
-    await ctx.send('知りたい内容があれば以下のコマンドを入力してください！')
-    
-    await ctx.send('/server')
-    await ctx.send('サーバーの概要')
-
-
-
-@bot.command()
-async def server1(ctx):
-    await ctx.send('このサーバーは有志によって作られたクラスサーバーです。')
-    await ctx.send('ここでは課題や予定の管理をはじめとし、様々なコミュニケーションを図れます。')
-
-bot.run("TOKEN")
-
-from discord.ext import commands
-import os
-import traceback
-
 class PagerWithEmojis:
     """
     受け取った絵文字によってページを移動するためのクラス
@@ -137,6 +100,35 @@ async def server(ctx: commands.Context):
     ]
     pager = PagerWithEmojis(pages)
     await pager.discord_pager(ctx)
+
+
+@bot.event
+async def on_command_error(ctx, error):
+    orig_error = getattr(error, "original", error)
+    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
+    await ctx.send(error_msg)
+
+@bot.command()
+async def begin(ctx):
+    await ctx.send('3MEサーバーへようこそ！')
+    await ctx.send('まずは「/tutorial」と入力して、サーバーについて学びましょう！')
+
+
+@bot.command()
+async def tutorial(ctx):
+    await ctx.send('チュートリアルを開始します。')
+    await ctx.send('知りたい内容があれば以下のコマンドを入力してください！')
+    
+    await ctx.send('/server')
+    await ctx.send('サーバーの概要')
+
+
+
+@bot.command()
+async def server1(ctx):
+    await ctx.send('このサーバーは有志によって作られたクラスサーバーです。')
+    await ctx.send('ここでは課題や予定の管理をはじめとし、様々なコミュニケーションを図れます。')
+
 
 bot.run("TOKEN")
 
