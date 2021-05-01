3b2e723e2012eb17aef6de0611775e3138cd8b2a
すずらん <83231128+suzuran1080ti@users.noreply.github.com>
2021-05-02 02:43:32 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,105 +1,46 @@
-from discord.ext import commands
-import os
-import traceback
+# NicoNico
 
-bot = commands.Bot(command_prefix='/')
-token = os.environ['DISCORD_BOT_TOKEN']
+import discord
+from niconico_dl_async import NicoNico
 
+client = discord.Client()
 
-@bot.event
-async def on_command_error(ctx, error):
-    orig_error = getattr(error, "original", error)
-    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
-    await ctx.send(error_msg)
+# 起動したらメッセージ。
+@client.event
+async def on_ready():
+    print("起動しました。")
 
-class PagerWithEmojis:
-    
-    LEFT_ARROW: str = "\N{LEFTWARDS BLACK ARROW}\N{VARIATION SELECTOR-16}"
-    RIGHT_ARROW: str = "\N{BLACK RIGHTWARDS ARROW}\N{VARIATION SELECTOR-16}"
-    STOP: str = "\N{CROSS MARK}"
-    # 処理に必要な絵文字を変数として持っておく
+@client.event
+async def on_message(message):
+    # もしBotだったら反応しない。
+    if message.author.bot:
+        return
 
-    def __init__(self, pages:list[discord.Embed]):
-        self.page_index: int = 0
-        self.pages: list[discord.Embed] = pages
+    # メッセージの内容の最初が!playなら。
+    if message.content.startswith("!play"):
+        # チャンネルに入力中を送信する。
+        await message.channel.trigger_typing()
+        # もしボイスチャンネルに接続していないなら、コマンド実行者のボイスチャンネルに接続する。
+        if not message.guild.voice_client:
+            await message.author.voice.channel.connect()
+        # メッセージの内容を/で分けてできたリストの最後を取得する。
+        # urlの最後にニコニコ動画のIDがあるはずだからそれをniconico_dlに渡す。
+        nico_id = message.content.split("/")[-1]
+        niconico = NicoNico(nico_id)
+        # 動画のURLを取得する。
+        url = await niconico.get_download_link()
+        # 動画のURLからオーディオソースを作る。(ffmpegが必要)
+        player = discord.FFmpegPCMAudio(url)
+        # 再生する。 
+        # playのafterという引数に終了時に実行してほしいものを入れた関数を入れることができる。
+        # niconico_dlはurlの使用が終わった後にclose()しなければいけない。
+        # なのでclose()をしてくれる関数をafterに入れる。
+        # 入れる関数にはエラー時のエラーメッセージをとる。
+        # 今回はlambdaを使って関数を作る。(lambdaについてはここを参考にしよう: https://qiita.com/nagataaaas/items/531b1fc5ce42a791c7df)
+        after = lambda e: niconico.close()
+        message.guild.voice_client.play(player, after=after)
+        # 通知メッセージ。
+        await message.channel.send("再生します。")
 
-    @property
-    def now_page(self) -> discord.Embed:
-        return self.pages[self.page_index]
-
-    @property
-    def max_page_index(self) -> int:
-        return len(self.pages) - 1
-
-    @property
-    def page_emojis(self) -> list[str]:
-        
-        emojis: list[str] = [self.LEFT_ARROW,self.RIGHT_ARROW,self.STOP]
-        if self.page_index == 0:
-            # もし、今最初のページにいるなら左へ移動する絵文字を除外する
-            emojis.remove(self.LEFT_ARROW)
-        if self.page_index == self.max_page_index:
-            # もし、今最後のページにいるなら右へ移動する絵文字を除外する
-            emojis.remove(self.RIGHT_ARROW)
-        return emojis
-
-    def move_page_by_emoji(self, emoji: str):
-        
-        if emoji == self.LEFT_ARROW:
-            self.page_index -= 1
-        elif emoji == self.RIGHT_ARROW:
-            self.page_index += 1
-
-    async def discord_pager(self, ctx: commands.Context):
-        
-        now_page = self.now_page
-        emojis = self.page_emojis
-        # 現在ページ内容と、出力に必要な絵文字を取得
-        msg: discord.Message = await ctx.send(embed=now_page)
-
-        for emoji in emojis:
-            await msg.add_reaction(emoji)
-
-        def check(reaction: discord.Reaction, user: discord.User) -> bool:
-            # リアクション先のメッセージや追加された絵文字が適切かどうか判断する。
-            return str(reaction.emoji) in emojis and reaction.message == msg and user == ctx.author
-
-        while True:
-            reaction, _ = await ctx.bot.wait_for("reaction_add",check=check)
-            await msg.clear_reactions()  # 事前に全てのリアクションを削除しておく。
-            if str(reaction.emoji) == self.STOP:
-                # 停止用の絵文字が追加された場合、リアクションを新たに付与することなく終了する。
-                break
-            self.move_page_by_emoji(str(reaction.emoji))
-            now_page = self.now_page
-            emojis = self.page_emojis
-            await msg.edit(embed=now_page)
-
-            for emoji in emojis:
-                await msg.add_reaction(emoji)
-
-
-@bot.command()
-async def page_test(ctx: commands.Context):
-    pages: list[discord.Embed] = [
-        discord.Embed(
-            title="1ページ目です。",
-            description="最初のページなので、右矢印とバツマークのリアクションが追加されます。",
-            color=discord.Color.random()
-        ),
-        discord.Embed(
-            title="2ページ目です。",
-            description="左矢印と右矢印に、バツマークのリアクションも追加されます。",
-            color=discord.Color.random()
-        ),
-        discord.Embed(
-            title="最後のページです。",
-            description="最後のページなので、左矢印とバツマークのリアクションが追加されます。",
-            color=discord.Color.random()
-        )
-    ]
-    pager = PagerWithEmojis(pages)
-    await pager.discord_pager(ctx)
-
-
-bot.run(token)
+# 接続する。
+client.run("TOKEN")
