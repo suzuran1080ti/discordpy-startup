a4849e1c4004e0f24ed36257d1d2dd9281f05201
すずらん <83231128+suzuran1080ti@users.noreply.github.com>
2021-05-08 17:00:23 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -15,14 +15,15 @@ async def on_command_error(ctx, error):
 @bot.command()
 async def begin(ctx):
     await ctx.send('3MEサーバーへようこそ！')
-    await ctx.send('まずは「/tutorial」と入力して、サーバーについて学びましょう！')
+    await ctx.send('まずは「/tutorial」と入力してサーバーについて学びましょう！')
 
 
 @bot.command()
 async def tutorial(ctx):
     await ctx.send('チュートリアルを開始します！')
     await ctx.send('知りたい内容があれば以下のコマンドを入力してください！')
- 
+    await ctx.send('知りたい内容が無い場合は上記の利用規約をよく読んだうえで「/next」と入力してください！')
+    
     await ctx.send('/server')
     await ctx.send('サーバーの概要')
     await ctx.send('/authority')
@@ -36,7 +37,7 @@ async def tutorial(ctx):
 @bot.command()
 async def server(ctx):
     await ctx.send('このサーバーは有志によって作られたクラスサーバーです。')
-    await ctx.send('ここでは課題や予定の管理をはじめとし、様々な交流を図れます。')
+    await ctx.send('ここでは課題や予定の管理をはじめとし、様々な交流が図れます。')
     await ctx.send('サーバー内には権限を管理する「笛」によって出現するチャンネルや「寮生専用チャット」「イブニングトーク」等の非公開チャンネルも存在します。')
     
     
@@ -49,13 +50,16 @@ async def authority(ctx):
 @bot.command()
 async def whistle(ctx):
     await ctx.send('笛はサーバーでの活動の評価値に応じて付与され、階級が上がるごとに機能が解禁されていきます。')
-
+    await ctx.send('笛の種類と条件については上記の資料をご覧ください。')
     await ctx.send('主に解禁される機能（変更あり）')
     await ctx.send('青笛・上位ボイスチャンネルの使用/コマンド部屋の閲覧')
     await ctx.send('月笛・招待リンクの作成/サーバーミュート/コマンド部屋の使用')
     await ctx.send('黒笛・チャンネル改変/ユーザーのキック/ニックネーム変更') 
     
       
-
+@bot.command()
+async def next(ctx):
+    await ctx.send('利用規約の承認を確認しました！')
+    await ctx.send('管理者がニックネームとロールを付与致しますので、自分の氏名と出席番号を入力してお待ちください！')
 
 bot.run(token)
