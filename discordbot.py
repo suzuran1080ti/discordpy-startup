222b5fe796ffae4839da95606c6d671de2abf878
すずらん <83231128+suzuran1080ti@users.noreply.github.com>
2021-05-08 16:37:28 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -29,8 +29,6 @@ async def tutorial(ctx):
     await ctx.send('サーバー権限')
     await ctx.send('/whistle')
     await ctx.send('笛の種類')
-    await ctx.send('/terms')
-    await ctx.send('利用規約')
     await ctx.send('/next')
     await ctx.send('次へ進む')
 
@@ -38,21 +36,21 @@ async def tutorial(ctx):
 @bot.command()
 async def server(ctx):
     await ctx.send('このサーバーは有志によって作られたクラスサーバーです。')
-    await ctx.send('ここでは課題や予定の管理をはじめとし、様々なコミュニケーションを図れます。')
-    await ctx.send('また、サーバー内には権限を管理する「笛」によって出現するチャンネルや「寮生専用チャット」「イブニングトーク」等の非公開チャンネルも存在します。')
+    await ctx.send('ここでは課題や予定の管理をはじめとし、様々な交流を図れます。')
+    await ctx.send('サーバー内には権限を管理する「笛」によって出現するチャンネルや「寮生専用チャット」「イブニングトーク」等の非公開チャンネルも存在します。')
     
     
 @bot.command()
 async def authority(ctx):
     await ctx.send('このサーバーでは笛によって階級が定められています。')
-    await ctx.send('詳細は/whistleで確認できますが、通常の機能を使うのであれば「鈴付き」で申し分ありません。')
+    await ctx.send('詳細は上記の表や/whistleで確認できますが、通常の機能を使うのであれば「鈴付き」で申し分ありません。')
 
 
 @bot.command()
 async def whistle(ctx):
-    await ctx.send('笛はサーバーでの活動の評価値に準じて付与され、階級が上がるごとに機能が解禁されていきます。')
+    await ctx.send('笛はサーバーでの活動の評価値に応じて付与され、階級が上がるごとに機能が解禁されていきます。')
 
-    await ctx.send('主に解禁される機能（頻繁に変更します）')
+    await ctx.send('主に解禁される機能（変更あり）')
     await ctx.send('青笛・上位ボイスチャンネルの使用/コマンド部屋の閲覧')
     await ctx.send('月笛・招待リンクの作成/サーバーミュート/コマンド部屋の使用')
     await ctx.send('黒笛・チャンネル改変/ユーザーのキック/ニックネーム変更') 
